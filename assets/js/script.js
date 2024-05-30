ab8d493b9ea4fa53e953c0fc9677461f008dff7c
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-05-30 22:36:44 +0630

    time-intervals

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+var message =
+  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
+var words = message.split(' ');
+
+//Timer for counts down
+function countdown() {
+  var timeLeft = 5;
+
+  // TODO: Add a comment describing the functionality of the setInterval() method:
+  // Set the times of every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // TODO: Add comments describing the functionality of the `if` statement:
+    // If 'timeLeft' is greater than 1, it reduce 1
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } // TODO: Add comments describing the functionality of the `else if` statement:
+    // If 'timeLeft' is equal to 1, change the displaying text
+    else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } // TODO: Add comments describing the functionality of the `else` statement:
+    // If 'timeLeft' is equal to 0,set 'timeEl' to empty
+    else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);   
+}
+
+// Displays the message one word at a time
+function displayMessage() {
+  var wordCount = 0;
+
+  // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var msgInterval = setInterval(function () {
+    // If there are no more words left in the message
+    if (words[wordCount] === undefined) {
+      // Use `clearInterval()` to stop the timer
+      clearInterval(msgInterval);
+    } else {
+      // Display one word of the message
+      mainEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 1000);
+}
+
+countdown();
